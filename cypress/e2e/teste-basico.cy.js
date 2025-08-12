2b801f084e74f812742a0a6ec3825501d89429da
Rychardsson <rychardssong2@gmail.com>
2025-08-12 15:24:09 -0300

    feat: remove deprecated Cypress configuration and server files

--- cypress/e2e/teste-basico.cy.js
+++
@@ -1,6 +0,0 @@
-describe("Teste Simples", () => {
-  it("Deve verificar se a configuração está funcionando", () => {
-    cy.log("Teste básico executando...");
-    expect(true).to.eq(true);
-  });
-});
