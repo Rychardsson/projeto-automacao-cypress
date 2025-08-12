13728aba41e385c2129027eb2d68220792de79d3
Rychardsson <rychardssong2@gmail.com>
2025-08-12 14:30:28 -0300

    feat: remove deprecated server and database files, add server startup script

--- cypress/e2e/teste-basico.cy.js
+++
@@ -1,7 +0,0 @@
-describe('Teste Básico do Cypress', () => {
-  it('Deve funcionar corretamente', () => {
-    cy.visit('https://example.com')
-    cy.contains('Example Domain')
-    cy.log('✅ Cypress está funcionando!')
-  })
-})
